2645dbf49591294192ace0e6ed1341f5b6ecaa88
Anton <gish2008@gmail.com>
2022-07-03 17:43:08 +0300

    remote-exec - script

--- boxfuze_app/boxfuze.tf
+++ boxfuze_app/boxfuze.tf
@@ -41,21 +41,14 @@ resource "aws_instance" "build_server" {
     source      = "/root/.ssh/config"
     destination = "/home/ubuntu/config"
   }
+    provisioner "file" {
+    source      = "provision_build.sh"
+    destination = "/tmp/provision_build.sh"
+  }
   provisioner "remote-exec" {
     inline = [
-"mkdir -p /home/ubuntu/.aws/",
-"mkdir -p /home/ubuntu/.ssh/",
-"mv /home/ubuntu/credentials /home/ubuntu/.aws/credentials",
-"mv /home/ubuntu/id_rsa /home/ubuntu/.ssh/id_rsa",
-"chmod 400 /home/ubuntu/.ssh/id_rsa",
-"mv /home/ubuntu/config /home/ubuntu/.ssh/config",
-"chmod 600 /home/ubuntu/.ssh/config",
-"git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'",
-"git clone ssh://APKAVNWETNK3NSW6CY4P@git-codecommit.us-east-1.amazonaws.com/v1/repos/boxfuze /tmp/boxfuze",
-"cd /tmp/boxfuze/",
-"echo $PWD",
-"mvn package",
-"aws s3 cp /tmp/boxfuze/target/hello-1.0.war s3://boxfuze.avasekho.test/hello-1.0.war",
+      "chmod +x /tmp/provision_build.sh",
+      "/tmp/provision_build.sh",
     ]
   }
 }
---
+++ boxfuze_app/provision_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p /home/ubuntu/.aws/
+mkdir -p /home/ubuntu/.ssh/
+mv /home/ubuntu/credentials /home/ubuntu/.aws/credentials
+mv /home/ubuntu/id_rsa /home/ubuntu/.ssh/id_rsa
+chmod 400 /home/ubuntu/.ssh/id_rsa
+mv /home/ubuntu/config /home/ubuntu/.ssh/config
+chmod 600 /home/ubuntu/.ssh/config
+git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+git clone ssh://APKAVNWETNK3NSW6CY4P@git-codecommit.us-east-1.amazonaws.com/v1/repos/boxfuze /tmp/boxfuze
+cd /tmp/boxfuze/
+mvn package
+aws s3 cp /tmp/boxfuze/target/hello-1.0.war s3://boxfuze.avasekho.test/hello-1.0.war
