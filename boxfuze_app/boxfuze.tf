e6c565fda053b8d949b4ed33184c37c7f90bdfec
Anton <gish2008@gmail.com>
2022-07-03 17:37:49 +0300

    .

--- boxfuze_app/boxfuze.tf
+++ boxfuze_app/boxfuze.tf
@@ -52,9 +52,10 @@ resource "aws_instance" "build_server" {
 "chmod 600 /home/ubuntu/.ssh/config",
 "git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'",
 "git clone ssh://APKAVNWETNK3NSW6CY4P@git-codecommit.us-east-1.amazonaws.com/v1/repos/boxfuze /tmp/boxfuze",
-"cd /tmp/boxfuze",
-"/usr/share/maven/bin/mvn package",
-"/usr/bin/aws s3 cp /tmp/boxfuze/target/hello-1.0.war s3://boxfuze.avasekho.test/hello-1.0.war",
+"cd /tmp/boxfuze/",
+"echo $PWD",
+"mvn package",
+"aws s3 cp /tmp/boxfuze/target/hello-1.0.war s3://boxfuze.avasekho.test/hello-1.0.war",
     ]
   }
 }
