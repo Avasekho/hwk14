f51249258c5997cec2d665eb94dc66cbe8b08056
Anton <gish2008@gmail.com>
2022-07-03 16:49:23 +0300

    .

--- boxfuze_app/build_data.sh
+++ boxfuze_app/build_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt update -y
 apt install -y maven default-jdk awscli
+mkdir /home/ubuntu/.aws
+mkdir /home/ubuntu/.ssh
--- boxfuze_app/prod_data.sh
+++ boxfuze_app/prod_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt update -y
 apt install -y tomcat9 awscli
+mkdir /home/ubuntu/.aws
+mkdir /home/ubuntu/.ssh
